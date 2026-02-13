19c0eb6498ce60898531d759472af469f16dceb9
FarhanAli995 <alyyfarhan4@gmail.com>
2026-02-13 14:22:14 +0500

    still learning about templates and having Django-Template-language Classes

--- gs13/course/views.py
+++ gs13/course/views.py
@@ -1,15 +1,37 @@
+# views.py
+
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+import random
+from datetime import datetime
 
 def learn_dj(request):
-    django_details ={
-        'nm': 'Django is Awesome',
-        'du':'4 Months'
     
+    courses = [
+        {"name": "Django Basics", "duration": "1 Month", "level": "Beginner"},
+        {"name": "Django REST API", "duration": "1.5 Months", "level": "Intermediate"},
+        {"name": "Django Deployment", "duration": "2 Weeks", "level": "Advanced"},
+    ]
+
+    student_count = random.randint(100, 500)
+    progress = random.randint(60, 100)
+
+    context = {
+        "title": "Advanced Django Course",
+        "courses": courses,
+        "students": student_count,
+        "progress": progress,
+        "today": datetime.now(),
+        "images": [
+            "https://picsum.photos/300/200?random=1",
+            "https://picsum.photos/300/200?random=2",
+            "https://picsum.photos/300/200?random=3",
+            "https://picsum.photos/300/200?random=4",
+        ]
     }
-    return render(request, 'course/courseone.html', django_details)
+
+    return render(request, "course/courseone.html", context)
+
 
 def learn_py(request):
-    return HttpResponse("python!")
+    return HttpResponse("Python Advanced Course Coming Soon 🚀")
