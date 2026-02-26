b16b8d29e8eaf91b2c640a35a861f1e685cfdf65
FarhanAli995 <alyyfarhan4@gmail.com>
2026-02-26 21:26:02 +0500

    Today I have Practice ORM and DATABASE and learned how to convert that python ORM code into SQL code through some cammands and more.

--- gs19/core/models.py
+++ gs19/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    
+    stuid = models.IntegerField(primary_key=True)
+    stuname = models.CharField(max_length=70)
+    stumail = models.CharField(max_length=70)
+    stupass = models.CharField(max_length=70)
+    comment = models.CharField(max_length=50, default="Not Commented")
+    
+
