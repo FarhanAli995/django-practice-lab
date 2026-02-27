899375971b1bed38796c0e8a00726e4e2d677880
FarhanAli995 <alyyfarhan4@gmail.com>
2026-02-27 20:13:05 +0500

    Retrieve Data (Fetch from Database)

--- gs19/core/models.py
+++ gs19/core/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+# class Students(models.Model):
+    
+#     stuid = models.IntegerField(primary_key=True)
+#     stuname = models.CharField(max_length=70)
+#     stumail = models.CharField(max_length=70)
+#     stupass = models.CharField(max_length=70)
+#     comment = models.CharField(max_length=50, default="Not Commented")
+    
+
 class Students(models.Model):
-    
-    stuid = models.IntegerField(primary_key=True)
-    stuname = models.CharField(max_length=70)
-    stumail = models.CharField(max_length=70)
-    stupass = models.CharField(max_length=70)
-    comment = models.CharField(max_length=50, default="Not Commented")
-    
+    name = models.CharField(max_length=100, default="Unknown")
+    age = models.IntegerField(default=0)
+
+
+
 
--- gs19/core/views.py
+++ gs19/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from . models import Students
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'core/home.html')
+    students = Students.objects.all()
+    return render(request, 'core/home.html', {"students": students})
