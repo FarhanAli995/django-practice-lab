784e50b5ec86800d8224970cea6879d35bd14441
FarhanAli995 <alyyfarhan4@gmail.com>
2026-03-01 12:02:49 +0500

    still learning database

--- gs20/course/models.py
+++ gs20/course/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class DjangoCourse(models.Model):
+    courseName = models.CharField(max_length=70)
+    courseFee = models.IntegerField()
+    seatsAvailable = models.IntegerField()
+    courseID = models.IntegerField()
+
+    def __str__(self):
+        return self.courseName
--- gs20/course/views.py
+++ gs20/course/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . models import DjangoCourse
 
 # Create your views here.
+
+def course(request):
+    djangoCourse = DjangoCourse.objects.all()
+    return render(request, 'course/course.html',{'djangoCourse': djangoCourse})
