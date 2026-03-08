d253363bf47a6800a3181957bc829847cbbeed44
FarhanAli995 <alyyfarhan4@gmail.com>
2026-03-08 11:10:33 +0500

    created homepage and added about named template

--- khanakhazana/core/urls.py
+++ khanakhazana/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),  
-    path('about/', views.about, name='about'),
-    path('footer/', views.footer, name='footer')
+    path('ab/', views.about, name='about'),
+    path('foot/', views.footer, name='footer')
 ]
--- khanakhazana/khanakhazana/urls.py
+++ khanakhazana/khanakhazana/urls.py
@@ -5,6 +5,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Include the URLs from the core app
-    path('about/', include('core.urls')),  # Include the URLs from the core app for about page
-    path('footer/', include('core.urls')),
 ]
