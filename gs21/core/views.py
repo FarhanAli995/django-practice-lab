f04682bf596fed58fe52aadc7f3386b137b68688
FarhanAli995 <alyyfarhan4@gmail.com>
2026-03-31 13:03:39 +0500

    Update gs21 project

--- gs21/core/views.py
+++ gs21/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def function(request):
+    template_name = 'template.html'
+    context = {}
+
+    if request.method == 'GET':
+        pass
+
+    if request.method == 'POST':
+        pass
+
+    return render(request, template_name, context)
