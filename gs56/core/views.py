1ec7c610ff51bc7899946c28b3cdc1ae4fdc2be6
FarhanAli995 <alyyfarhan4@gmail.com>
2026-06-21 22:52:06 +0500

    Added a new template of congratulation of submission of form through same function.

--- gs56/core/views.py
+++ gs56/core/views.py
@@ -5,7 +5,12 @@ def showdata(request):
     if request.method == "POST":
         fm = StuReg(request.POST)
         if fm.is_valid():
+            name = fm.cleaned_data["name"]
+            email = fm.cleaned_data["email"]
             print("Form is valid")
+            
+        return render(request, "core/success.html", {"form": fm})
     else:
         fm = StuReg()
     return render(request, "core/home.html", {"form": fm})
+
