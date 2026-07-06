22fec95c5f8c4671a8a08732435eede0b4cb0f12
FarhanAli995 <alyyfarhan4@gmail.com>
2026-07-06 22:06:31 +0500

    Day 2 of learning about custom path converters

--- gs67/gs67/urls.py
+++ gs67/gs67/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/', include('root.urls')),
+    path('', include('root.urls')),
 ]
--- gs67/root/urls.py
+++ gs67/root/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('show/', views.show_student, name='show'),
+    path('', views.home, name='home'),
 ]
