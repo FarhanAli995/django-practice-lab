411ccc45f8ee11bdbcc451dfb7250a56bc0dda63
FarhanAli995 <alyyfarhan4@gmail.com>
2026-07-19 22:02:34 +0500

    learning about msg levels

---
+++ gs71_msg_levels/root/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from . import forms
+
+# Create your views here.
+
+
+def student_success_page(request):
+    return render(request, 'root/student_success.html')
+
+
+def teacher_success_page(request):
+    return render(request, 'root/teacher_success.html')
+
+
+class StudentFormView(View):
+    template_name = 'root/student.html'
+    form_class = forms.Student_Registration
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            # messages.add_message(request, messages.SUCCESS, 'Student registered successfully!')
+            messages.success(request, 'Student registered successfully!')
+            messages.info(request, 'now you can log in')
+        return render(request, self.template_name, {'form': form})
+ 
+
+class TeacherFormView(View):
+    template_name = 'root/teacher.html'
+    form_class = forms.Teacher_Registration
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Teacher registered successfully!')
+            
+        return render(request, self.template_name, {'form': form})
